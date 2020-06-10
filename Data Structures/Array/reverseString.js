48f9db909ea293c1a8995b7c3fa69abae770188a
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-06-10 22:40:35 +0000

    added conditions to handle errors on reverseString.js

--- Data Structures/Array/reverseString.js
+++ Data Structures/Array/reverseString.js
@@ -1,7 +1,12 @@
 function reverse(string) {
+    if(Number(string) || !string) return 'please pass valid input';
+    if(string.length < 2) return string;
+
     let reversedString = ''
+    
     for(let i = string.length - 1; i >= 0; i--) {
         reversedString += string[i];
     }
+    
     return reversedString;
 }
