0e061b8588aab111c85a0e604ad2595d40179189
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-06-10 22:36:48 +0000

    finished reverseString.js

---
+++ Data Structures/Array/reverseString.js
@@ -0,0 +1,7 @@
+function reverse(string) {
+    let reversedString = ''
+    for(let i = string.length - 1; i >= 0; i--) {
+        reversedString += string[i];
+    }
+    return reversedString;
+}
