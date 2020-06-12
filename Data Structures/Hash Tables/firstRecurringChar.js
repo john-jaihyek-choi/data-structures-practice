64b3a359ab12935e6106f75f1532727079564741
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-06-12 01:51:11 +0000

    completed implementation of linear complexity solution for firstRecurringChar.js

--- Data Structures/Hash Tables/firstRecurringChar.js
+++ Data Structures/Hash Tables/firstRecurringChar.js
@@ -14,7 +14,13 @@
 
 // Brainstorm:
 // Naive solution: Quadratic time complexity (O(n^2));
+    // keep track of temp value for the value at index
+    // keep track of smallestDistance
     // simply loop nest two loops to check for all possible occurances
+    // and set a condition for two things:
+        // if i'th value at arr and j'th value at arr is equal
+            // AND
+        // if tempDist is smaller than smallestDistance;
 
 
 // Pseudocode (Naive Solution):
@@ -25,10 +31,39 @@
                 // if true, return value of arr at i'th index;
     // return undefined if nothing is returned from the above function
 
-function firstRecurrence(arr) {
+function firstRecurrence(arr) { // Time / Space Complexity
+    if(!arr) return undefined;
+    let temp = 0;
+    let smallestDistance = arr.length;
     for(let i = 0; i < arr.length; i++) {
+        let tempDist = 0;
         for(let j = i+1; j < arr.length; j++) {
-            if(arr[i] === arr[j]) return arr[i];
+            tempDist++;
+            if(arr[i] === arr[j] && tempDist < smallestDistance){ // O(n^2) / O(1);
+                smallestDistance = tempDist;
+                temp = arr[i];
+            }
+        }
+    }
+    return temp;
+}
+
+// Brainstorm:
+// Second approach: Linear time complexity
+    // Some possible approach:
+        // hashTables: tradeoff between space and time;
+            // loop through the arr initially and create a key value pair for all values
+                // during loop, if we find existing value,
+                    // return the key of that value
+
+function firstRecurrence(arr) { // Time / Space Complexity
+    const hashObj = {};
+    for(let i = 0; i < arr.length; i++) { // O(n) / O(n);
+        console.log(i, arr[i]);
+        if(hashObj[arr[i]]) {
+            return arr[i];
+        } else {
+            hashObj[arr[i]] = i+1;
         }
     }
     return undefined;
